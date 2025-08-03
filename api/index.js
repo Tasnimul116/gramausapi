5d8a5b49aa9454a3220318754817249a8e45b95f
Tasnimul Hasan Mahi <mahitasnimul2@gmail.com>
2025-08-04 00:29:07 +0600

    fix deploy issue

--- api/index.js
+++ api/index.js
@@ -1,3 +1,17 @@
+// api/index.js
+const { createServer, proxy } = require('aws-serverless-express');
+const app = require('../dist/app').default;
 
-const app = require("../dist/app");
-module.exports = app;
+let server;
+
+function getServer() {
+  if (!server) {
+    server = createServer(app);
+  }
+  return server;
+}
+
+module.exports = (req, res) => {
+  const server = getServer();
+  return proxy(server, req, res);
+};
