07dca392318118f6da954ed107aa41601f3b9d7f
Tasnimul Hasan Mahi <mahitasnimul2@gmail.com>
2025-08-04 01:19:45 +0600

    fix issue

--- src/server.ts
+++ src/server.ts
@@ -5,57 +5,88 @@ import config from "./app/config";
 
 let server: Server;
 
-// async function main() {
-//   try {
-//    await mongoose.connect(config.database_url as string);
-
-//     server = app.listen(config.port, () => {
-//       console.log(`app is listening on port ${config.port}`);
-//     });
-
-//   } catch (err) {
-//     console.log(err);
-//   }
-// }
-
-// main();
-
-// process.on("unhandledRejection", (err) => {
-//   console.log(`😈 unahandledRejection is detected , shutting down ...`, err);
-//   if (server) {
-//     server.close(() => {
-//       process.exit(1);
-//     });
-//   }
-//   process.exit(1);
-// });
-
-// process.on("uncaughtException", () => {
-//   console.log(`😈 uncaughtException is detected , shutting down ...`);
-//   process.exit(1);
-// });
-
-
-
 async function main() {
   try {
-    await mongoose.connect(config.database_url as string);
+   await mongoose.connect(config.database_url as string);
+
     server = app.listen(config.port, () => {
       console.log(`app is listening on port ${config.port}`);
     });
+
+
+    // initializeSocket(server);
+        
+    // const io = require("socket.io")(server, {
+    //   pingTimeout: 60000,
+    //   cors: {
+    //     origin: "*",
+    //     methods: ["GET", "POST"],
+    //   },
+    // });
+
+
+    // io.on("connection", (socket: any) => {
+    //   // console.log("connected to socket.io");
+    //   // socket.on("setup", (userData: any) => {
+    //   //   socket.join(userData._id);
+    //   //   // console.log(userData._id);
+    //   //   socket.emit("connected");
+    //   // });
+    //   // socket.on("join chat", (room: any) => {
+    //   //   socket.join(room);
+    //   //   // console.log("user joined room " + room);
+    //   // });
+    //   // socket.on("typing", (typer: any) => {
+    //   //   const {room, user} = typer;
+    //   //   socket.broadcast.to(room).emit("typing", typer);
+    //   // });
+    //   // socket.on("stop typing", (typer: any) => {
+    //   //   const {room, user} = typer;
+    //   //   socket.broadcast.to(room).emit("stop typing", typer);
+    //   // });
+
+    //   // socket.on("new message", (newMessageReceived: any) => {
+    //   //   let chat = newMessageReceived?.data?.data;
+    //   //   // console.log(chat)
+    //   //   if (!chat.authorId) {
+    //   //     return console.log("chat.users not defined");
+    //   //   }
+    //   //   // chat?.users?.forEach((user: any) => {
+    //   //   //   if (user?._id === newMessageReceived.sender?._id) {
+    //   //   //     return;
+    //   //   //   }
+    //   //   // });
+    //   //   else if(chat?.authorId !== chat?.otherUser){
+          
+    //   //     socket
+    //   //       .in(chat.otherUser)
+    //   //       .emit("message received", newMessageReceived);
+    //   //   }
+    //   //   if (chat?.otherUserArr) {
+    //   //     chat?.otherUserArr.forEach((user: any) => {
+    //   //       socket.in(user?._id).emit("message received", newMessageReceived);
+
+    //   //     });
+          
+    //   //   }
+    //   // }
+    //   // );
+
+
+    //   // socket.off("setup", (userData: any) => {
+    //   //   socket.leave(userData._id);
+    //   // });
+
+    // });
   } catch (err) {
     console.log(err);
   }
 }
 
-// Only start the server if this file is run directly (not imported)
-if (require.main === module) {
-  main();
-}
+main();
 
-// Graceful shutdown
 process.on("unhandledRejection", (err) => {
-  console.log(`😈 unhandledRejection is detected, shutting down ...`, err);
+  console.log(`😈 unahandledRejection is detected , shutting down ...`, err);
   if (server) {
     server.close(() => {
       process.exit(1);
@@ -65,9 +96,6 @@ process.on("unhandledRejection", (err) => {
 });
 
 process.on("uncaughtException", () => {
-  console.log(`😈 uncaughtException is detected, shutting down ...`);
+  console.log(`😈 uncaughtException is detected , shutting down ...`);
   process.exit(1);
 });
-
-// ✅ Add this line at the very end
-export default app;
